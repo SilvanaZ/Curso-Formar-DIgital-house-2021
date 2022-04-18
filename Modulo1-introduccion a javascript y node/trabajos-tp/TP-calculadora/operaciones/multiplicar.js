76904af175101526e32da73235038cca2eedfcda
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-04-18 20:20:51 -0300

    Update multiplicar.js

--- Modulo1-introduccion a javascript y node/trabajos-tp/TP-calculadora/operaciones/multiplicar.js
+++ Modulo1-introduccion a javascript y node/trabajos-tp/TP-calculadora/operaciones/multiplicar.js
@@ -1,6 +1,4 @@
 function multiplicar(n1 = 1,n2 = 1){
-    let parametro1 = n1;
-    let parametro2 = n2;
     if(a === undefined || b === undefined ){
         return "Tienes que pasarme dos parámetros"
     } else if (typeof a !== "number"|| typeof b !== "number"){
