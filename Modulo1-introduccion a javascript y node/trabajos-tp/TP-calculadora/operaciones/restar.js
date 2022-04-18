279b1687874bca062b8275c5ce44e5c54f898dad
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-04-18 20:21:46 -0300

    Update restar.js

--- Modulo1-introduccion a javascript y node/trabajos-tp/TP-calculadora/operaciones/restar.js
+++ Modulo1-introduccion a javascript y node/trabajos-tp/TP-calculadora/operaciones/restar.js
@@ -1,6 +1,4 @@
 function restar(n1 = 0, n2 = 0){
-    let parametro1 = n1;
-    let parametro2 = n2;
     if(a === undefined || b === undefined ){
         return "Tienes que pasarme dos parámetros"
     } else if (typeof a !== "number"|| typeof b !== "number"){
